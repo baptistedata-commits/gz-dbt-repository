d86ce7b65f861485cd63bef32f94f5ab426162a5
baptiste.data@gmail.com <baptiste.data@gmail.com>
2025-07-29 12:33:12 +0000

    orders_operational

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,8 @@
+SELECT
+orders_id,
+date_date,
+margin + shipping_fee - logcost - ship_cost AS Operational_margin
+FROM {{ ref("int_orders_margin")}}
+JOIN {{ ref("stg_raw__ship")}} USING (orders_id)
+ORDER BY orders_id DESC
+
