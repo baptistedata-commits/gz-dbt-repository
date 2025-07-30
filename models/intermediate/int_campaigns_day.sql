0e27d657d22a584c16168c41526c47357f3cb7cc
baptiste.data@gmail.com <baptiste.data@gmail.com>
2025-07-30 10:01:05 +0000

    campaign day simplifié

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,11 +1,9 @@
 SELECT
 date_date,
 paid_source,
-campaign_key,
-campaign_name,
 SUM(ads_cost) AS total_ads_cost,
 SUM(impression) AS total_impression,
 SUM(click) AS total_click
 FROM {{ref("int_campaigns")}}
-GROUP BY date_date, paid_source, campaign_key, campaign_name
+GROUP BY date_date, paid_source
 ORDER BY date_date DESC
